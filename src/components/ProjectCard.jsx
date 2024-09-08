abcf51bdd716d360633c882bd2d6d135362a045d
Gebexe <gpalacios93@hotmail.com>
2024-09-08 15:07:32 -0300

    agregado estilos para projectCard, arreglado es.json y agregado projecto

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './ProjectCard.css'
 
 function ProjectCard({ src, link, h3, p }) {
   return (
     <a href={link} target="_blank" rel="noreferrer">
-      <img className="hover" src={src} alt={`${h3} logo`} />
-      <h3>{h3}</h3>
-      <p>{p}</p>
+      <div className='box'>
+      <img className="ProjectLogo" src={src} alt={`${h3} logo`} />
+      <h3 className='ProjectH'>{h3}</h3>
+      <p className='ProjectP'>{p}</p>
+      </div>
     </a>
   );
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,16 +1,18 @@
 import styles from './ProjectsStyles.module.css';
+import logo from '../../assets/OBS_Studio_logo.png'
 import ProjectCard from '../ProjectCard';
 
 export function Projects() {
   return (
     <section id="projects" className={styles.container}>
-      <h1 className="sectionTitle">Projects</h1>
+      <h1 className={styles.projectTitle}>Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src="https://media.tenor.com/damu8hbJ19YAAAAe/shrug-emoji.png"
-          link="https://twitch.com"
-          h3="Nada"
-          p="Streaming App"
+          src={logo}
+          link="https://apiscoreboards.vercel.app/"
+          h3="Api Scoreboard para OBS"
+          p={`Overlay de scores para juegos de pelea para ser utilizado en apps de streaming
+            como OBS se debe utilizar filtro de color #0056b3 para la transparencia`}
         />
         
       </div>
